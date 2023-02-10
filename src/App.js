6afd023aa4171b744a8b08ff3d069f6629862095
CorinaVizitiu <corina.vizitiu@yahoo.com>
2023-02-10 09:59:52 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import image from './contact.png'
+function App() {
+  return (
+    <div className="App">
+   
+     <h1>Bienvenue</h1>
+     <img src={image} alt="image" />
+    </div>
+  );
+}
+
+export default App;
